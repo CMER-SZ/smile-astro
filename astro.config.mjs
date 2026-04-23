824b346db919d4c282a385a5be836061a87837d0
谭洁莹 <blacktree@qq.com>
2026-04-23 17:18:47 +0800

    build(config): astro新增sitemap，tailwind新增配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
-
+  site: 'https://smile.hkcmereye.com',
+  integrations: [vue(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
