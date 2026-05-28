ba2f2fe3538aac3a5579ecab3c075d0581d5bc64
谭洁莹 <blacktree@qq.com>
2026-05-28 17:40:01 +0800

    feat(i18n): about独立文件

--- src/i18n/zh-cn/index.ts
+++ src/i18n/zh-cn/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: 谭洁莹
+ * @Date: 2026-04-24 18:42:52
+ * @LastEditTime: 2026-05-28 17:39:21
+ * @FilePath: /src/i18n/zh-cn/index.ts
+ * @Description: 统一导出
+ */
+import { about } from "./about";
 import { common } from "./common";
 import { home } from "./home";
 import { page } from "./page";
@@ -5,6 +13,7 @@ import { service } from "./service";
 import { tdk } from "./tdk";
 
 export const zhCN = {
+  about,
   common,
   home,
   page,
--- src/i18n/zh-hk/index.ts
+++ src/i18n/zh-hk/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: 谭洁莹
+ * @Date: 2026-04-24 18:42:52
+ * @LastEditTime: 2026-05-28 17:38:56
+ * @FilePath: /src/i18n/zh-hk/index.ts
+ * @Description: 统一导出
+ */
+import { about } from "./about";
 import { common } from "./common";
 import { home } from "./home";
 import { page } from "./page";
@@ -5,6 +13,7 @@ import { service } from "./service";
 import { tdk } from "./tdk";
 
 export const zhHK = {
+  about,
   common,
   home,
   page,
